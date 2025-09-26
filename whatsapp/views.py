5019d86af9f7ebaa925f769e2a3154779a8acfbc
Kauan dos Santos de Souza <kauan.souza@orsegups.com.br>
2025-09-26 09:16:18 -0300

    Adicionado o login_required na view da message e alterado o script do message.html

--- whatsapp/views.py
+++ whatsapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from whatsapp.form.WhatsForm import WhatsCustomForm
 from django.contrib import messages
+from usuarios.utils.LoginRequired import login_required_session
 
+@login_required_session
 def message(request):
     if request.method == 'POST':
         form = WhatsCustomForm(request.POST)
